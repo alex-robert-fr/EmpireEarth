fcb0981d851cc38ac0f3bdb3c0321ebc86bf366b
Alex Robert <contact.alexrobert04@gmail.com>
2024-12-26 15:27:16 +0100

    ✨ Add 3D Camera

--- main.c
+++ main.c
@@ -2,10 +2,29 @@
 
 int main() {
   InitWindow(800, 600, "Empire Earth - The New Age");
+
+  Camera3D camera = {0};
+  camera.position = (Vector3){10.0f, 10.0f, 10.0f};
+  camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+  camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+  camera.fovy = 45.0f;
+  camera.projection = CAMERA_PERSPECTIVE;
+
+  Vector3 cube_position = {0.0f, 0.0f, 0.0f};
+  DisableCursor();
+  SetTargetFPS(60);
+
   while (!WindowShouldClose()) {
+    UpdateCamera(&camera, CAMERA_FREE);
+    if (IsKeyPressed('Z'))
+      camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("Hello world!", 190, 200, 20, LIGHTGRAY);
+    BeginMode3D(camera);
+    DrawCube(cube_position, 2.0f, 2.0f, 2.0f, RED);
+    DrawGrid(10, 1.0f);
+    EndMode3D();
     EndDrawing();
   }
   CloseWindow();
